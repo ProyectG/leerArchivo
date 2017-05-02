b14aa113df0e120bebf8abf9dc0f12f9a60df21f
proyectg <marco.saffie@gmail.com>
2017-05-01 21:41:48 -0300

    Se agrega un retorno en hashmap para luego mostrarlo.

--- lectura_archivo/src/lectura_archivo/inicio.java
+++ lectura_archivo/src/lectura_archivo/inicio.java
@@ -1,6 +1,8 @@
 package lectura_archivo;
 
 import java.io.IOException;
+import java.util.HashMap;
+
 import lectura_archivo.leer;
 
 public class inicio {
@@ -9,13 +11,25 @@ public class inicio {
 	
 	@SuppressWarnings("unused")
 	leer Archivo = new leer();
+	HashMap<String,String> resultado = null;
 	try {
-		leer.leerArchivo("prueba.txt");
+		resultado = leer.leerArchivo("prueba.txt");
 	} catch (IOException e) {
 		// TODO Auto-generated catch block
 		e.printStackTrace();
+		System.out.println("No se ha encontrado ningun archivo.");
 	}
 		
+	
+	if(resultado == null)
+	{
+		//Hacer algo? que, no se me ocurre.
+	}
+	else
+	{
+		System.out.println(resultado);
+	}	
+	
 	}
 
 }
--- lectura_archivo/src/lectura_archivo/leer.java
+++ lectura_archivo/src/lectura_archivo/leer.java
@@ -4,17 +4,32 @@ import java.io.BufferedReader;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
+import java.util.HashMap;
 
 public class leer {
 	
-	 public static void leerArchivo(String archivo) throws FileNotFoundException, IOException {
+	 @SuppressWarnings("null")
+	public static HashMap<String,String> leerArchivo(String archivo) throws FileNotFoundException, IOException {
 	        String cadena;
+	        int contador=1;
+	        HashMap<String,String> resultado = null;
+	        try
+	        {
 	        FileReader f = new FileReader(archivo);
 	        BufferedReader b = new BufferedReader(f);
 	        while((cadena = b.readLine())!=null) {
 	            System.out.println(cadena);
+	        	resultado.put("Linea "+contador,cadena);
+	        	contador++;
 	        }
 	        b.close();
+	        }
+	        catch(Exception e)
+	        {
+	        	e.printStackTrace();
+	        }
+	        return resultado;
 	    }
+	 
 
 }
